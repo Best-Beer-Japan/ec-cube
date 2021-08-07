bd59624e1106d2d175322021744e9130c84f14b2
Bell <dev.belltzel@gmail.com>
2021-08-07 23:26:55 +0900

    ビアスタイルに関する初期データを追加するmigrationを追加

---
+++ app/DoctrineMigrations/Version20210807140417.php
@@ -0,0 +1,85 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210807140417 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $date = (new \DateTime())->format('Y-m-d H:i:s');
+
+        // 商品CSV
+        $csvExists = $this->connection->fetchColumn("SELECT COUNT(*) FROM dtb_csv WHERE csv_type_id = 1 AND entity_name = 'Eccube\\\\Entity\\\\Product' AND field_name = 'BeerStyle' AND reference_field_name = 'name' AND disp_name = 'ビアスタイル'");
+        if ($csvExists == 0) {
+            $this->addSql("INSERT INTO dtb_csv (
+                id, csv_type_id, creator_id, entity_name, field_name, reference_field_name, disp_name, sort_no, enabled, create_date, update_date, discriminator_type
+            ) VALUES (
+                NULL, 1, NULL, 'Eccube\\\\Entity\\\\Product', 'BeerStyle', 'name', 'ビアスタイル', 1, 0, ?, ?, 'csv'
+            )",
+                [
+                    $date,
+                    $date
+                ]);
+        }
+
+        //　受注CSV
+        $csvExists = $this->connection->fetchColumn("SELECT COUNT(*) FROM dtb_csv WHERE csv_type_id = 3 AND entity_name = 'Eccube\\\\Entity\\\\OrderItem'
+                               AND field_name = 'Product' AND reference_field_name = 'BeerStyle' AND disp_name = 'ビアスタイル'");
+        if ($csvExists == 0) {
+            $this->addSql("INSERT INTO dtb_csv (
+                id, csv_type_id, creator_id, entity_name, field_name, reference_field_name, disp_name, sort_no, enabled, create_date, update_date, discriminator_type
+            ) VALUES (
+                NULL, 3, NULL, 'Eccube\\\\Entity\\\\OrderItem', 'Product', 'BeerStyle', 'ビアスタイル', 1, 0, ?, ?, 'csv'
+            )",
+                [
+                    $date,
+                    $date
+                ]);
+        }
+
+        // 配送CSV
+        $csvExists = $this->connection->fetchColumn("SELECT COUNT(*) FROM dtb_csv WHERE csv_type_id = 4 AND entity_name = 'Eccube\\\\Entity\\\\OrderItem'
+                               AND field_name = 'Product' AND reference_field_name = 'BeerStyle' AND disp_name = 'ビアスタイル'");
+        if ($csvExists == 0) {
+            $this->addSql("INSERT INTO dtb_csv (
+                id, csv_type_id, creator_id, entity_name, field_name, reference_field_name, disp_name, sort_no, enabled, create_date, update_date, discriminator_type
+            ) VALUES (
+                NULL, 4, NULL, 'Eccube\\\\Entity\\\\OrderItem', 'Product', 'BeerStyle', 'ビアスタイル', 1, 0, ?, ?, 'csv'
+            )",
+                [
+                    $date,
+                    $date
+                ]);
+        }
+
+        // ビアスタイル（マスター）
+        $beerTypeExists = $this->connection->fetchColumn("SELECT COUNT(*) FROM customize_mtb_beer_style WHERE 1");
+        if ($beerTypeExists == 0) {
+            $this->addSql("INSERT INTO customize_mtb_beer_style (
+                id, name, sort_no, discriminator_type
+            ) VALUES ('1', 'Lager / Pilsner', '1', 'beerstyle'),
+                    ('2', 'White Ale/Weizen', '2', 'beerstyle'),
+                    ('3', 'Ale Beer', '3', 'beerstyle'),
+                    ('4', 'IPA', '4', 'beerstyle'),
+                    ('5', 'Black Beer', '5', 'beerstyle'),
+                    ('6', 'Fruit & Herb/Flavor Beer', '6', 'beerstyle'),
+                    ('7', 'Sour / Gose', '7', 'beerstyle'),
+                    ('8', 'Special Beer', '8', 'beerstyle'),
+                    ('9', 'その他', '9', 'beerstyle')"
+            );
+        }
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
