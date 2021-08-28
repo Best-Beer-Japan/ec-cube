782f1c3f5c7c5402bfa2fcb2ed61224a30b43696
Bell <dev.belltzel@gmail.com>
2021-08-29 05:52:00 +0900

    新着リリースブロックmigrationに登録チェック追加

--- app/DoctrineMigrations/Version20210826082847.php
+++ app/DoctrineMigrations/Version20210826082847.php
@@ -12,11 +12,20 @@ final class Version20210826082847 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
-        $this->addSql("INSERT INTO `dtb_block` (
-                `id`, `device_type_id`, `block_name`, `file_name`, `use_controller`, `deletable`, `create_date`, `update_date`, `discriminator_type`
+        $date = (new \DateTime())->format('Y-m-d H:i:s');
+
+        $blockExists = $this->connection->fetchColumn("SELECT COUNT(*) FROM dtb_block WHERE file_name = 'corpse_tag_style'");
+        if ($blockExists == 0) {
+            $this->addSql("INSERT INTO dtb_block (
+                id, device_type_id, block_name, file_name, use_controller, deletable, create_date, update_date, discriminator_type
             ) VALUES (
-                NULL, 10, '最新リリース一覧', 'corpse_new_release', 1, 0, NOW(), NOW(), 'block'
-            )");
+                NULL, 10, 'スタイル一覧', 'corpse_tag_style', 1, 0, ?, ?, 'block'
+            )",
+                [
+                    $date,
+                    $date
+                ]);
+        }
     }
 
     public function down(Schema $schema) : void
