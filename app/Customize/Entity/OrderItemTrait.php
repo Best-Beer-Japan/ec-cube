fb69ac3a2f3048a7e91b02bb7146df0e7f5a8168
Bell <dev.belltzel@gmail.com>
2021-09-11 05:46:21 +0900

    original_idカラムにnullを許可する。

--- app/Customize/Entity/OrderItemTrait.php
+++ app/Customize/Entity/OrderItemTrait.php
@@ -13,7 +13,7 @@ trait OrderItemTrait
     /**
      * @var int
      *
-     * @ORM\Column(name="original_id", type="integer", options={"unsigned":true})
+     * @ORM\Column(name="original_id", type="integer", nullable=true, options={"unsigned":true})
      */
     private $original_id;
 
