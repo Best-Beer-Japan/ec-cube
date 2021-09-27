0c2ecad2ebd20325a258ead65123c069feeae289
mako <tano@bestbeerjapan.com>
2021-09-28 08:25:17 +0900

    ClassNameへbrewery_idを追加

--- app/Customize/Entity/ClassNameTrait.php
+++ app/Customize/Entity/ClassNameTrait.php
@@ -13,9 +13,40 @@ trait ClassNameTrait
     /**
      * @var int
      *
+     * @ORM\Column(name="brewery_id", type="integer", nullable=true, options={"unsigned":true})
+     */
+    private $brewery_id;
+
+    /**
+     * @var int
+     *
      * @ORM\Column(name="original_id", type="integer", nullable=true, options={"unsigned":true})
      */
     private $original_id;
+
+    /**
+     * Set brewery_id.
+     *
+     * @param int|null $brewery_id
+     *
+     * @return self
+     */
+    public function setBreweryId(?int $brewery_id): self
+    {
+        $this->brewery_id = $brewery_id;
+
+        return $this;
+    }
+
+    /**
+     * Get bbj_payment_id.
+     *
+     * @return int|null
+     */
+    public function getBreweryId(): ?int
+    {
+        return $this->brewery_id;
+    }
 
     /**
      * Set original_id.
