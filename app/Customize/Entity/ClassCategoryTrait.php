d1a7518927f828e98146d2a49d353be8e62c16da
mako <tano@bestbeerjapan.com>
2021-09-28 08:37:18 +0900

    ClassCategoryへoriginal_idを追加

---
+++ app/Customize/Entity/ClassCategoryTrait.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Customize\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Eccube\Annotation\EntityExtension;
+
+/**
+ * @EntityExtension("Eccube\Entity\ClassCategory")
+ */
+trait ClassCategoryTrait
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="original_id", type="integer", nullable=true, options={"unsigned":true})
+     */
+    private $original_id;
+
+    /**
+     * Set original_id.
+     *
+     * @param int|null $original_id
+     *
+     * @return self
+     */
+    public function setOriginalId(?int $original_id): self
+    {
+        $this->original_id = $original_id;
+
+        return $this;
+    }
+
+    /**
+     * Get original_id.
+     *
+     * @return int|null
+     */
+    public function getOriginalId(): ?int
+    {
+        return $this->original_id;
+    }
+}
