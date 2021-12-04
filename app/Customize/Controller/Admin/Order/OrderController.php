a43fc3fcd505ba1adcc18f336c0d1444bff63001
Bell <dev.belltzel@gmail.com>
2021-12-05 03:26:14 +0900

    請求月の設定機能を追加

---
+++ app/Customize/Controller/Admin/Order/OrderController.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace Customize\Controller\Admin\Order;
+
+use Eccube\Common\Constant;
+use Eccube\Controller\AbstractController;
+use Eccube\Entity\ExportCsvRow;
+use Eccube\Entity\Master\CsvType;
+use Eccube\Entity\Master\OrderStatus;
+use Eccube\Entity\Order;
+use Eccube\Entity\OrderPdf;
+use Eccube\Entity\Shipping;
+use Eccube\Event\EccubeEvents;
+use Eccube\Event\EventArgs;
+use Eccube\Form\Type\Admin\OrderPdfType;
+use Eccube\Form\Type\Admin\SearchOrderType;
+use Eccube\Repository\CustomerRepository;
+use Eccube\Repository\Master\OrderStatusRepository;
+use Eccube\Repository\Master\PageMaxRepository;
+use Eccube\Repository\Master\ProductStatusRepository;
+use Eccube\Repository\Master\SexRepository;
+use Eccube\Repository\OrderPdfRepository;
+use Eccube\Repository\OrderRepository;
+use Eccube\Repository\PaymentRepository;
+use Eccube\Repository\ProductStockRepository;
+use Eccube\Service\CsvExportService;
+use Eccube\Service\MailService;
+// use Eccube\Service\OrderPdfService;
+use Customize\Service\OrderCustomPdfService as OrderPdfService;
+use Eccube\Service\OrderStateMachine;
+use Eccube\Service\PurchaseFlow\PurchaseFlow;
+use Eccube\Util\FormUtil;
+use Knp\Component\Pager\PaginatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class OrderController extends AbstractController
+{
+    /**
+     * @var PurchaseFlow
+     */
+    protected $purchaseFlow;
+
+    /**
+     * @var CsvExportService
+     */
+    protected $csvExportService;
+
+    /**
+     * @var CustomerRepository
+     */
+    protected $customerRepository;
+
+    /**
+     * @var PaymentRepository
+     */
+    protected $paymentRepository;
+
+    /**
+     * @var SexRepository
+     */
+    protected $sexRepository;
+
+    /**
+     * @var OrderStatusRepository
+     */
+    protected $orderStatusRepository;
+
+    /**
+     * @var PageMaxRepository
+     */
+    protected $pageMaxRepository;
+
+    /**
+     * @var ProductStatusRepository
+     */
+    protected $productStatusRepository;
+
+    /**
+     * @var OrderRepository
+     */
+    protected $orderRepository;
+
+    /** @var OrderPdfRepository */
+    protected $orderPdfRepository;
+
+    /**
+     * @var ProductStockRepository
+     */
+    protected $productStockRepository;
+
+    /** @var OrderPdfService */
+    protected $orderPdfService;
+
+    /**
+     * @var ValidatorInterface
+     */
+    protected $validator;
+
+    /**
+     * @var OrderStateMachine
+     */
+    protected $orderStateMachine;
+
+    /**
+     * @var MailService
+     */
+    protected $mailService;
+
+    /**
+     * OrderController constructor.
+     *
+     * @param PurchaseFlow $orderPurchaseFlow
+     * @param CsvExportService $csvExportService
+     * @param CustomerRepository $customerRepository
+     * @param PaymentRepository $paymentRepository
+     * @param SexRepository $sexRepository
+     * @param OrderStatusRepository $orderStatusRepository
+     * @param PageMaxRepository $pageMaxRepository
+     * @param ProductStatusRepository $productStatusRepository
+     * @param ProductStockRepository $productStockRepository
+     * @param OrderRepository $orderRepository
+     * @param OrderPdfRepository $orderPdfRepository
+     * @param ValidatorInterface $validator
+     * @param OrderStateMachine $orderStateMachine ;
+     */
+    public function __construct(
+        PurchaseFlow $orderPurchaseFlow,
+        CsvExportService $csvExportService,
+        CustomerRepository $customerRepository,
+        PaymentRepository $paymentRepository,
+        SexRepository $sexRepository,
+        OrderStatusRepository $orderStatusRepository,
+        PageMaxRepository $pageMaxRepository,
+        ProductStatusRepository $productStatusRepository,
+        ProductStockRepository $productStockRepository,
+        OrderRepository $orderRepository,
+        OrderPdfRepository $orderPdfRepository,
+        ValidatorInterface $validator,
+        OrderStateMachine $orderStateMachine,
+        MailService $mailService
+    ) {
+        $this->purchaseFlow = $orderPurchaseFlow;
+        $this->csvExportService = $csvExportService;
+        $this->customerRepository = $customerRepository;
+        $this->paymentRepository = $paymentRepository;
+        $this->sexRepository = $sexRepository;
+        $this->orderStatusRepository = $orderStatusRepository;
+        $this->pageMaxRepository = $pageMaxRepository;
+        $this->productStatusRepository = $productStatusRepository;
+        $this->productStockRepository = $productStockRepository;
+        $this->orderRepository = $orderRepository;
+        $this->orderPdfRepository = $orderPdfRepository;
+        $this->validator = $validator;
+        $this->orderStateMachine = $orderStateMachine;
+        $this->mailService = $mailService;
+    }
+
+    /**
+     * Update to order status
+     *
+     * @Route("/%eccube_admin_route%/order/{id}/billing_date", requirements={"id" = "\d+"}, name="admin_order_update_billing_date", methods={"PUT"})
+     *
+     * @param Request $request
+     * @param Order $Order
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function updateOrderBilling(Request $request, Order $Order)
+    {
+        if (!($request->isXmlHttpRequest() && $this->isTokenValid())) {
+            return $this->json(['status' => 'NG'], 400);
+        }
+
+        $year = $request->get('billing_year');
+        $month = $request->get('billing_month');
+
+        $NowDateTime = new \DateTime('NOW');
+        $startYear = $NowDateTime->format('Y') - 1;
+        $endYear = $startYear + 2;
+
+        if (($year < $startYear || $endYear < $year) || ($month < 1 || 12 < $month)) {
+            return $this->json(['status' => 'NG'], 400);
+        }
+
+        $result = [];
+        try {
+            $setDate = new \DateTime();
+            $setDate->setDate($year, $month, 1)->setTime(0, 0, 0);
+
+            $Order->setCustomizeBillingMonthDate($setDate);
+            $this->entityManager->persist($Order);
+            $this->entityManager->flush();
+
+            log_info('請求月一括変更処理完了', [$Order->getId()]);
+        } catch (\Exception $e) {
+            log_error('予期しないエラーです', [$e->getMessage()]);
+
+            return $this->json(['status' => 'NG'], 500);
+        }
+
+        return $this->json(array_merge(['status' => 'OK'], $result));
+    }
+}
