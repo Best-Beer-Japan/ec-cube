34e9ef6f1fdbf6b00f26b34953324e13e3dee223
Yousuke Hayashi <48919150+belltzel@users.noreply.github.com>
2022-01-27 14:26:03 +0900

    請求月に関わる機能 (#108)
    * 請求月の設定機能を追加
    
    * 請求月を範囲で絞り込み検索する条件を追加
    
    * 請求CSVのダウンロード機能追加
    
    * 受注一覧検索の検索条件、請求月の修正
    
    * 検索後も条件がフォームに保存されるように修正
    
    * 請求月の解除ボタン、機能を追加

---
+++ app/Customize/Controller/Admin/Order/OrderController.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace Customize\Controller\Admin\Order;
+
+use Eccube\Controller\AbstractController;
+use Eccube\Entity\ExportCsvRow;
+use Eccube\Entity\Order;
+use Eccube\Event\EccubeEvents;
+use Eccube\Event\EventArgs;
+use Eccube\Repository\Master\CsvTypeRepository;
+use Eccube\Service\CsvExportService;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrderController extends AbstractController
+{
+    /**
+     * @var CsvExportService
+     */
+    protected $csvExportService;
+
+    /**
+     * @var CsvTypeRepository
+     */
+    protected $csvTypeRepository;
+
+    /**
+     * OrderController constructor.
+     *
+     * @param CsvExportService $csvExportService
+     * @param CsvTypeRepository $csvTypeRepository
+     */
+    public function __construct(
+        CsvExportService $csvExportService,
+        CsvTypeRepository $csvTypeRepository
+    ) {
+        $this->csvExportService = $csvExportService;
+        $this->csvTypeRepository = $csvTypeRepository;
+    }
+
+    /**
+     * Update to order status
+     *
+     * @Route("/%eccube_admin_route%/order/{id}/billing_date", requirements={"id" = "\d+"}, name="admin_order_update_billing_date", methods={"PUT"})
+     *
+     * @param Request $request
+     * @param Order $Order
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function updateOrderBilling(Request $request, Order $Order)
+    {
+        if (!($request->isXmlHttpRequest() && $this->isTokenValid())) {
+            return $this->json(['status' => 'NG'], 400);
+        }
+
+        $year = $request->get('billing_year');
+        $month = $request->get('billing_month');
+
+        $NowDateTime = new \DateTime('NOW');
+        $startYear = $NowDateTime->format('Y') - 1;
+        $endYear = $startYear + 2;
+
+        if (($year < $startYear || $endYear < $year) || ($month < 1 || 12 < $month)) {
+            return $this->json(['status' => 'NG'], 400);
+        }
+
+        $result = [];
+        try {
+            $setDate = new \DateTime();
+            $setDate->setDate($year, $month, 1)->setTime(0, 0, 0);
+
+            $Order->setCustomizeBillingMonthDate($setDate);
+            $this->entityManager->persist($Order);
+            $this->entityManager->flush();
+
+            log_info('請求月一括変更処理完了', [$Order->getId()]);
+        } catch (\Exception $e) {
+            log_error('予期しないエラーです', [$e->getMessage()]);
+
+            return $this->json(['status' => 'NG'], 500);
+        }
+
+        return $this->json(array_merge(['status' => 'OK'], $result));
+    }
+
+    /**
+     * Update to order status
+     *
+     * @Route("/%eccube_admin_route%/order/{id}/billing_date/lift", requirements={"id" = "\d+"}, name="admin_order_lift_billing_date", methods={"PUT"})
+     *
+     * @param Request $request
+     * @param Order $Order
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function liftOrderBilling(Request $request, Order $Order)
+    {
+        if (!($request->isXmlHttpRequest() && $this->isTokenValid())) {
+            return $this->json(['status' => 'NG'], 400);
+        }
+
+        try {
+            $Order->setCustomizeBillingMonthDate(null);
+            $this->entityManager->persist($Order);
+            $this->entityManager->flush();
+
+            log_info('請求月一括変更処理完了', [$Order->getId()]);
+        } catch (\Exception $e) {
+            log_error('予期しないエラーです', [$e->getMessage()]);
+
+            return $this->json(['status' => 'NG'], 500);
+        }
+
+        return $this->json(array_merge(['status' => 'OK']));
+    }
+
+    /**
+     * 請求CSVの出力.
+     *
+     * @Route("/%eccube_admin_route%/customize/order/export/billing_date", name="admin_customize_order_export_billing_date")
+     *
+     * @param Request $request
+     *
+     * @return StreamedResponse
+     */
+    public function exportShipping(Request $request)
+    {
+        $filename = 'billing_date_'.(new \DateTime())->format('YmdHis').'.csv';
+        $CsvType = $this->csvTypeRepository->findOneBy(['name' => '請求CSV']);
+        $response = $this->exportCsv($request, $CsvType->getId(), $filename);
+        log_info('請求CSV出力ファイル名', [$filename]);
+
+        return $response;
+    }
+
+    /**
+     * @param Request $request
+     * @param $csvTypeId
+     * @param string $fileName
+     *
+     * @return StreamedResponse
+     */
+    protected function exportCsv(Request $request, $csvTypeId, $fileName)
+    {
+        // タイムアウトを無効にする.
+        set_time_limit(0);
+
+        // sql loggerを無効にする.
+        $em = $this->entityManager;
+        $em->getConfiguration()->setSQLLogger(null);
+
+        $response = new StreamedResponse();
+        $response->setCallback(function () use ($request, $csvTypeId) {
+            // CSV種別を元に初期化.
+            $this->csvExportService->initCsvType($csvTypeId);
+
+            // ヘッダ行の出力.
+            $this->csvExportService->exportHeader();
+
+            // 受注データ検索用のクエリビルダを取得.
+            $qb = $this->csvExportService
+                ->getOrderQueryBuilder($request);
+
+            $qb->leftJoin('o.Customer', 'c');
+            $qb->orderBy('c.id', 'ASC');
+            $qb->addorderBy('o.id', 'ASC');
+
+            // データ行の出力.
+            $this->csvExportService->setExportQueryBuilder($qb);
+            $this->csvExportService->exportData(function ($entity, $csvService) use ($request) {
+                $Csvs = $csvService->getCsvs();
+
+                $Order = $entity;
+                $OrderItems = $Order->getOrderItems();
+
+                foreach ($OrderItems as $OrderItem) {
+                    $ExportCsvRow = new ExportCsvRow();
+
+                    // CSV出力項目と合致するデータを取得.
+                    foreach ($Csvs as $Csv) {
+                        // 受注データを検索.
+                        $ExportCsvRow->setData($csvService->getData($Csv, $Order));
+                        if ($ExportCsvRow->isDataNull()) {
+                            // 受注データにない場合は, 受注明細を検索.
+                            $ExportCsvRow->setData($csvService->getData($Csv, $OrderItem));
+                        }
+                        if ($ExportCsvRow->isDataNull() && $Shipping = $OrderItem->getShipping()) {
+                            // 受注明細データにない場合は, 出荷を検索.
+                            $ExportCsvRow->setData($csvService->getData($Csv, $Shipping));
+                        }
+                        if ($ExportCsvRow->isDataNull() && $Product = $OrderItem->getProduct()) {
+                            // 受注明細データにない場合は, 商品を検索.
+                            $ExportCsvRow->setData($csvService->getData($Csv, $Product));
+                        }
+
+                        $event = new EventArgs(
+                            [
+                                'csvService' => $csvService,
+                                'Csv' => $Csv,
+                                'OrderItem' => $OrderItem,
+                                'ExportCsvRow' => $ExportCsvRow,
+                            ],
+                            $request
+                        );
+                        $this->eventDispatcher->dispatch(EccubeEvents::ADMIN_ORDER_CSV_EXPORT_ORDER, $event);
+
+                        $ExportCsvRow->pushData();
+                    }
+
+                    //$row[] = number_format(memory_get_usage(true));
+                    // 出力.
+                    $csvService->fputcsv($ExportCsvRow->getRow());
+                }
+            });
+        });
+
+        $response->headers->set('Content-Type', 'application/octet-stream');
+        $response->headers->set('Content-Disposition', 'attachment; filename='.$fileName);
+        $response->send();
+
+        return $response;
+    }
+}
--- app/Customize/Form/Extension/AdminSearchOrderTypeExtension.php
+++ app/Customize/Form/Extension/AdminSearchOrderTypeExtension.php
@@ -6,6 +6,9 @@ use Eccube\Common\EccubeConfig;
 use Eccube\Form\Type\Admin\SearchOrderType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -37,6 +40,66 @@ class AdminSearchOrderTypeExtension extends AbstractTypeExtension
                 'class' => 'Eccube\Entity\Delivery',
                 'choice_label' => 'name',
                 'placeholder' => 'admin.common.select',
+            ])
+            ->add('customize_billing_date', ChoiceType::class, [
+                'label' => 'admin.order.customize_billing_date',
+                'placeholder' => '請求月の範囲を指定',
+                'choices' => [
+                    '未設定の請求月' => 1,
+                ],
+                'required' => false,
+                'expanded' => false,
+                'multiple' => false,
+            ])
+            ->add('order_customize_billing_date_start', DateType::class, [
+                'label' => 'admin.order.order_customize_billing_date__start',
+                'required' => false,
+                'input' => 'datetime',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'placeholder' => ['year' => '----', 'month' => '--', 'day' => '--'],
+                'attr' => [
+                    'class' => 'datetimepicker-input',
+                    'data-target' => '#'.$this->getBlockPrefix().'_order_customize_billing_date_start',
+                    'data-toggle' => 'datetimepicker',
+                ],
+            ])
+            ->add('order_customize_billing_datetime_start', DateTimeType::class, [
+                'label' => 'admin.order.order_customize_billing_date__start',
+                'required' => false,
+                'input' => 'datetime',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM',
+                'attr' => [
+                    'class' => 'datetimepicker-input-month',
+                    'data-target' => '#'.$this->getBlockPrefix().'_order_customize_billing_datetime_start',
+                    'data-toggle' => 'datetimepicker',
+                ],
+            ])
+            ->add('order_customize_billing_date_end', DateType::class, [
+                'label' => 'admin.order.order_customize_billing_date__start',
+                'required' => false,
+                'input' => 'datetime',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'placeholder' => ['year' => '----', 'month' => '--', 'day' => '--'],
+                'attr' => [
+                    'class' => 'datetimepicker-input',
+                    'data-target' => '#'.$this->getBlockPrefix().'_order_customize_billing_date_end',
+                    'data-toggle' => 'datetimepicker',
+                ],
+            ])
+            ->add('order_customize_billing_datetime_end', DateTimeType::class, [
+                'label' => 'admin.order.order_customize_billing_date__start',
+                'required' => false,
+                'input' => 'datetime',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM',
+                'attr' => [
+                    'class' => 'datetimepicker-input-month',
+                    'data-target' => '#'.$this->getBlockPrefix().'_order_customize_billing_datetime_end',
+                    'data-toggle' => 'datetimepicker',
+                ],
             ]);
     }
 
@@ -46,5 +109,13 @@ class AdminSearchOrderTypeExtension extends AbstractTypeExtension
     public function getExtendedType()
     {
         return SearchOrderType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'admin_search_order';
     }
 }
