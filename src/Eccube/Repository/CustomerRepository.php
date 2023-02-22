e37e19c351ea2afd2a101f6af4d1591af9e7c687
Bell <dev.belltzel@gmail.com>
2023-02-23 02:14:22 +0900

    会員一覧ページ項目ソート機能追加

--- src/Eccube/Repository/CustomerRepository.php
+++ src/Eccube/Repository/CustomerRepository.php
@@ -55,6 +55,11 @@ class CustomerRepository extends AbstractRepository
      */
     protected $encoderFactory;
 
+    public const COLUMNS = [
+        'customer_id' => 'c.id'
+        ,'name'=> 'c.name01'
+    ];
+
     /**
      * CustomerRepository constructor.
      *
@@ -279,7 +284,15 @@ class CustomerRepository extends AbstractRepository
         }
 
         // Order By
-        $qb->addOrderBy('c.update_date', 'DESC');
+        if (isset($searchData['sortkey']) && !empty($searchData['sortkey'])) {
+            $sortOrder = (isset($searchData['sorttype']) && $searchData['sorttype'] == 'a') ? 'ASC' : 'DESC';
+            $qb->orderBy(self::COLUMNS[$searchData['sortkey']], $sortOrder);
+            $qb->addOrderBy('c.update_date', 'DESC');
+            $qb->addOrderBy('c.id', 'DESC');
+        } else {
+            $qb->orderBy('c.update_date', 'DESC');
+            $qb->addOrderBy('c.id', 'DESC');
+        }
 
         return $this->queries->customize(QueryKey::CUSTOMER_SEARCH, $qb, $searchData);
     }
