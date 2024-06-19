0938cbb51f8610c21029b6b574bf12a8929e2050
Bell <dev.belltzel@gmail.com>
2024-06-20 08:08:16 +0900

    受注CSVに容器容量(ml)を追加

---
+++ app/DoctrineMigrations/Version20240619224356.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240619224356 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $maxSortNo = $this->connection->fetchColumn('SELECT MAX(sort_no) + 1 FROM dtb_csv WHERE csv_type_id = 3;');
+
+        $csvOrderCustomerGroupExists = $this->connection->fetchColumn("SELECT COUNT(*) FROM dtb_csv WHERE csv_type_id = 3 and field_name = 'ProductClass' and reference_field_name = 'beer_container_capacity_other_ml'");
+        if ($csvOrderCustomerGroupExists == 0) {
+            $this->addSql("INSERT INTO dtb_csv (csv_type_id, entity_name, field_name, reference_field_name, disp_name, sort_no, enabled, create_date, update_date, discriminator_type) VALUES
+            (3, 'Eccube\\\\Entity\\\\OrderItem', 'ProductClass', 'beer_container_capacity_other_ml', '容器容量(ml)', $maxSortNo, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'csv')");
+        }
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $deleteId = $this->connection->fetchColumn("SELECT id FROM dtb_csv WHERE csv_type_id = 3 and field_name = 'ProductClass' and reference_field_name = 'beer_container_capacity_other_ml'");
+        $this->addSql("DELETE FROM dtb_csv WHERE id = ?;",
+            [
+                $deleteId
+            ]);
+    }
+}
